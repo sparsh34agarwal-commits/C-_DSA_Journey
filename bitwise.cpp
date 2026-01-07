c119082a410c985b2c03c179bcaf8b3a1b7db43a
sparsh34agarwal-commits <sparsh.34.agarwal@gmail.com>
2026-01-07 19:52:51 +0530

    Learned bitwise today

---
+++ bitwise.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=6,b=5;
+    cout<<(a&b)<<endl;
+    cout<<(a|b)<<endl;
+    cout<<(~b)<<endl;
+    cout<<(a^b)<<endl;
+    cout<<(b<<2)<<endl;
+    cout<<(a>>3)<<endl;
+}
