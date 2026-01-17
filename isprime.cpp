211cb308a958e610d2c2518189f4c5fe02e918a6
sparsh34agarwal-commits <sparsh.34.agarwal@gmail.com>
2026-01-17 12:52:49 +0530

    implement prime number check

---
+++ isprime.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+bool isprime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0)return 0;
+    }
+    return 1;
+}
+int main(){
+    int n;
+    cin>>n;
+    if(n==1)cout<<"neither";
+    else if(isprime(n))cout<<"prime"<<endl;
+    else{cout<<"composite";}
+}
