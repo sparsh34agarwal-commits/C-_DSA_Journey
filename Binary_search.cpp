b17b8b892f70b12d7f725321e80445e445fdd536
sparsh34agarwal-commits <sparsh.34.agarwal@gmail.com>
2026-02-01 11:42:42 +0530

    finding index of an element in an array using Binary Search

---
+++ Binary_search.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int binarysearch(int k, int arr[], int n){
+   int s=0,e=12;
+   int mid=(s+e)/2;
+   for(int i=0;i<n;i++){
+     if(arr[mid]==k)return mid;
+     else if(arr[mid]>k){
+      e=mid-1;
+      mid=(s+e)/2;
+     }
+     else{s=mid+1;mid=(s+e)/2;}
+   }
+   return -1;
+}
+int main(){
+    int arr[12]={2,3,4,5,6,7,8,9,14,19,22,29};
+    int k=10294;
+    cout<<binarysearch(k,arr,12);
+}
